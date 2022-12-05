ff8bfbcb4b0ee5308af2534e7eaa592d97528375
Amancaya <koda01996@gmail.com>
2022-12-05 17:06:54 -0400

    Initial Commit

---
+++ Bullseye/BullseyeApp.swift
@@ -0,0 +1,17 @@
+//
+//  BullseyeApp.swift
+//  Bullseye
+//
+//  Created by Amancaya Iriarte on 5/12/22.
+//
+
+import SwiftUI
+
+@main
+struct BullseyeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
